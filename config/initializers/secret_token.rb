c0433d9f4f3019be50559a8f18a8dbd73495ed7a
br0nc080 <mwelbu2@gmail.com>
2011-04-17 00:59:50 -0500

    initial commit after exexecuting nifty:layout and devise:install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grouptalk::Application.config.secret_token = '0965a7eec8114ea6dbbde83c4efe40bdcc340e5801e8d111d70ac3c4d70784beed4696201c93c33371b40f8a23326eac509cd531952ec63ce4cd4dcb9ca691f3'
