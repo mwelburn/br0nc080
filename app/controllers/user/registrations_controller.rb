a5a1893071722bbd8aab419e1c5994c92c55469b
br0nc080 <mwelbu2@gmail.com>
2011-04-17 11:27:25 -0500

    Updated controllers to allow access to custom views

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,19 +1,5 @@
 class User
-  class Private
-    class RegistrationsController < Devise::RegistrationsController
+  class RegistrationsController < Devise::RegistrationsController
 
-    end
-  end
-
-  class Private
-    class SessionsController < Devise::SessionsController
-
-    end
-  end
-
-  class Private
-    class PasswordsController < Devise::PasswordsController
-      
-    end
   end
 end
