d97685f32095d6663ea4af4bb20c6915962f8338
br0nc080 <mwelbu2@gmail.com>
2011-04-24 22:53:39 -0500

    more uncommitted pieces from the previous commit

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,8 +1,8 @@
 class Relationship < ActiveRecord::Base
 
-  belongs_to :follower, :class_name => 'User'
-  belongs_to :followed, :class_name => 'User'
-  validates_uniqueness_of :follower_id, :scope => :user_id
+  belongs_to :follower, :class_name => 'User', :foreign_key => 'follower_id'
+  belongs_to :followed, :class_name => 'User', :foreign_key => 'followed_id'
+  validates_uniqueness_of :follower_id, :scope => :followed_id
   validates_presence_of :follower_id, :followed_id
 
 end
